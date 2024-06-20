679f7ea853c4302f768ff212cbbff042aa292217
Vikas Rathore <vikasrathore95160@gmail.com>
2024-06-20 21:46:47 +0530

    default_and_named_export
    default export & named export concepts in javascript

---
+++ my-second-app/src/module1.mjs
@@ -0,0 +1,8 @@
+import sannata, {a,c,d} from './module2.mjs'
+console.log(sannata);
+console.log(a);
+console.log(d);
+console.log(c);
+
+
+
---
+++ my-second-app/src/module2.mjs
@@ -0,0 +1,11 @@
+const a = "harry";
+const b = "vikas";
+const c = "raj";
+const d = "ganesh";
+
+export default b;
+export {a};
+export {c};
+export {d};
+
+
