b894babea306de1cea02584a4ea7c7911c154118
Vikas Rathore <vikasrathore95160@gmail.com>
2024-06-30 08:01:31 +0530

    lecture 8 & 9 completed
    added functionalities to textUtils website

--- my-second-app/src/components/TextForm.js
+++ my-second-app/src/components/TextForm.js
@@ -8,18 +8,40 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleLowerCaseClick = ()=>{
+        console.log("lowercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleClearClick = ()=>{
+        let newText = "";
+        setText(newText);
+    }
+
     const handleOnChange = (event)=>{
         console.log("onChange");
         setText(event.target.value);
     }
     return (
-        <div>
+        <>
+        <div className="container"> 
             <h3>{props.heading}</h3>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
             </div>
-
-            <button className="btn btn-primary" onClick={handleUpperCaseClick} >Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpperCaseClick} >Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowerCaseClick} >Convert to LowerCase</button>
+            <button className="btn btn-danger mx-1" onClick={handleClearClick} >Clear text</button>
         </div>
+        <div className="container my-3">
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{ Math.round( 0.008 * text.split(" ").length )} minutes read</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+        </div>
+        </>
+
     )
 }
